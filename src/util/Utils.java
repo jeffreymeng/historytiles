9546a2100153e084d348aff70788276de00ca9ac
Gravitonic <dylany360@gmail.com>
2018-03-26 09:08:53 -0700

    t

--- src/util/Utils.java
+++ src/util/Utils.java
@@ -1,5 +1,16 @@
 package util;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Utils {
+	
+	/**
+	 * @param min the minimum number to be generated (inclusive)
+	 * @param max the maximum number to be generated (exclusive)
+	 * @return a random number between min and max
+	 */
+	public static int randInt(int min, int max) {
+		return ThreadLocalRandom.current().nextInt(min, max);
+	}
 
 }
