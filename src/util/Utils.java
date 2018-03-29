84b503522c78ea3b13fd5cbb5b3abde48c524d8d
Jeffrey Meng <jeffkmeng@gmail.com>
2018-03-29 09:14:01 -0700

    sync

--- src/util/Utils.java
+++ src/util/Utils.java
@@ -16,4 +16,13 @@ public class Utils {
 		return (int)(Math.log10(number)+1);
 		//basically, log10 returns log base 10 of the number. Our number isn't exactly a multiple of 10(probably), so we cast it as an int.
 	}
+	public static int getDigit(int number, int digit) {
+		//The nth digit is (the remainder of dividing by 10^n) divided by 10^n-1
+		for (int i = 0; i < getDigits(number); i ++) {
+			
+		}
+		
+		return 0;//for now
+
+	}
 }
