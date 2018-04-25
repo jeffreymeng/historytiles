4d2dfcab59438760a6bff2b884cbb5da047e4aa3
Gravitonic <dylany360@gmail.com>
2018-04-25 10:15:56 -0700

    Finish EquationLevel hopefully

--- src/level/EquationLevel.java
+++ src/level/EquationLevel.java
@@ -22,7 +22,7 @@ public class EquationLevel extends Level {
 
 	private int numVariables;
 	private Object[] equation;
-	private int[][] hiddenDigits; // effectively an array of 3-tuples in format {index, answer} (index in equation)
+	private int[] hiddenDigits; // indexes (in equation) of hidden digits
 
 	public EquationLevel(int numVariables, String... formatModifiers) {
 		this(numVariables, new int[] {Utils.randInt(0, 10), Utils.randInt(0, 10), Utils.randInt(0, 10), Utils.randInt(0, 10), Utils.randInt(0, 10)}, formatModifiers);
@@ -57,7 +57,7 @@ public class EquationLevel extends Level {
 			equation[1] = new Operator(Operator.MULTIPLICATION);
 			equation[2] = new Digit(numbers[1]);
 			equation[3] = new Operator(Operator.EQUALS);
-			result = getDigitValue(0) * getDigitValue(2);
+			result = getValue(0) * getValue(2);
 
 
 		} else if (!coefficient && !multiplyLeft && constantLeft) {
@@ -68,7 +68,7 @@ public class EquationLevel extends Level {
 			equation[1] = new Operator(Operator.ADDITION);
 			equation[2] = new Digit(numbers[1]);
 			equation[3] = new Operator(Operator.EQUALS);
-			result = getDigitValue(0) + getDigitValue(2);
+			result = getValue(0) + getValue(2);
 			//equation[4] = new Digit(((Digit)equation[0]).getValue() + ((Digit)equation[2]).getValue());
 
 		}
@@ -87,7 +87,7 @@ public class EquationLevel extends Level {
 			equation[5] = new Digit(numbers[2]);
 			equation[6] = new Operator(Operator.CLOSE_PARENTHESES);
 			equation[7] = new Operator(Operator.EQUALS);
-			result = getDigitValue(0) * getDigitValue(3) * getDigitValue(5);
+			result = getValue(0) * getValue(3) * getValue(5);
 			resultIndex = 8;
 			//equation[8] = new Digit( ((Digit)equation[0]).getValue() * ((Digit)equation[3]).getValue() * ((Digit)equation[5]).getValue() );
 
@@ -101,7 +101,7 @@ public class EquationLevel extends Level {
 			equation[3] = new Operator(Operator.ADDITION);
 			equation[4] = new Digit(numbers[2]);
 			equation[5] = new Operator(Operator.EQUALS);
-			result = getDigitValue(0) * getDigitValue(2) * getDigitValue(4);
+			result = getValue(0) * getValue(2) * getValue(4);
 			resultIndex = 6;
 
 		} else if (!coefficient && multiplyLeft && constantLeft) {
@@ -116,7 +116,7 @@ public class EquationLevel extends Level {
 			equation[5] = new Digit(numbers[2]);
 			equation[6] = new Operator(Operator.CLOSE_PARENTHESES);
 			equation[7] = new Operator(Operator.EQUALS);
-			result = getDigitValue(0) * (getDigitValue(3) + getDigitValue(5));
+			result = getValue(0) * (getValue(3) + getValue(5));
 			resultIndex = 8;
 
 		}
@@ -137,7 +137,7 @@ public class EquationLevel extends Level {
 			equation[7] = new Digit(numbers[3]);
 			equation[8] = new Operator(Operator.OPEN_PARENTHESES);
 			equation[9] = new Operator(Operator.EQUALS);
-			result = getDigitValue(0) * ( getDigitValue(3) * getDigitValue(5) + getDigitValue(7) );
+			result = getValue(0) * ( getValue(3) * getValue(5) + getValue(7) );
 			resultIndex = 10;
 
 		}
@@ -147,7 +147,7 @@ public class EquationLevel extends Level {
 		else
 			equation[resultIndex] = new Digit(result);
 
-		hiddenDigits = new int[numVariables][2];
+		hiddenDigits = new int[numVariables];
 
 		addVariables();		
 	}
@@ -156,13 +156,9 @@ public class EquationLevel extends Level {
 		for (int i = 0; i < numVariables; i++) {
 			int randIndex = Utils.randInt(0, equation.length - 1);
 			if (equation[randIndex] instanceof Digit) {
-				((Digit)equation[randIndex]).setVisible(false);
-				hiddenDigits[i][0] = randIndex;
-				hiddenDigits[i][1] = ((Digit)equation[randIndex]).getValue();
+				((Digit) equation[randIndex]).setVisible(false);
 			} else if (equation[randIndex] instanceof Number) {
-				((Number)equation[randIndex]).setVisible(false);
-				hiddenDigits[i][0] = randIndex;
-				hiddenDigits[i][1] = ((Number)equation[randIndex]).getValue();
+				((Number) equation[randIndex]).setVisible(false);
 			} else
 				i--; // do not increment counter if randIndex did not select a Digit or Number (e.g. if an Operator was selected)
 			// for loop increments i automatically so decrementing i results in a net change of zero
@@ -170,21 +166,49 @@ public class EquationLevel extends Level {
 	}
 
 	public boolean fill(int index, int number) {
-		if (hiddenDigits[index][1] == number) {
-			if (equation[index] instanceof Digit) {
-				((Digit) equation[index]).setVisible(true);
-			} else if (equation[index] instanceof Number)
-				((Number) equation[index]).setVisible(true);
+		if (equation[index] instanceof Digit && ((Digit) equation[index]).getValue() == number) {
+			((Digit) equation[index]).setVisible(true);
+			return true;
+		} else if (equation[index] instanceof Number && ((Number) equation[index]).getValue() == number) {
+			((Number) equation[index]).setVisible(true);
 			return true;
 		} else
 			return false;
 	}
 
-	public int getDigitValue(int index) {
+	public int getValue(int index) {
 		if (equation[index] instanceof Digit)
-			return ((Digit)equation[index]).getValue();
+			return ((Digit) equation[index]).getValue();
+		else if (equation[index] instanceof Number)
+			return ((Number) equation[index]).getValue();
 		else
 			return -1;
+	}
+	
+	public Object getEquationComponent(int index) {
+		return equation[index];
+	}
+
+	public int[] getAnswers() {
+		int[] answers = new int[numVariables];
+		for (int i = 0; i < answers.length; i++)
+			answers[i] = -1;
+		for (int i = 0; i < equation.length; i++) {
+			if (equation[i] instanceof Digit && ((Digit) equation[i]).isVisible() == false) {
+				for (int j = 0; j < answers.length; j++) // for each number in answers
+					if (answers[j] == -1) { // if slot is unfilled
+						answers[j] = ((Digit) equation[i]).getValue(); // fill slot
+						break; // exit loop so only one slot is filled
+					} // if slot is already filled loop continues and looks at next slot
+			} else if (equation[i] instanceof Number && ((Number) equation[i]).isVisible() == false) {
+				for (int j = 0; j < answers.length; j++)
+					if (answers[j] == -1) { // same logic as above
+						answers[j] = ((Number) equation[i]).getValue();
+						break;
+					}
+			}
+		}
+		return answers;
 	}
 
 	public String toString() {
